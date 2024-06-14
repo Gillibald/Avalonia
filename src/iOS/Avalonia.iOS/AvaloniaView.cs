b78d53d9af30671f17819b04b318e3594bef8e6b
Benedikt Stebner <Gillibald@users.noreply.github.com>
2024-06-14 12:09:33 +0200

    Fix iOS

--- src/iOS/Avalonia.iOS/AvaloniaView.cs
+++ src/iOS/Avalonia.iOS/AvaloniaView.cs
@@ -221,6 +221,8 @@ namespace Avalonia.iOS
                 // No-op
             }
 
+            public double DesktopScaling => RenderScaling;
+            public IScreenImpl? Screen { get; }
             public IPlatformHandle? Handle { get; }
             public Size ClientSize => new Size(_view.Bounds.Width, _view.Bounds.Height);
             public Size? FrameSize => null;
