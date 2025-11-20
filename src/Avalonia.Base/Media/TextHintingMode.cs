500c76e6cb0dedc41e7ff71dba24fa302b0b24fb
Benedikt Stebner <Gillibald@users.noreply.github.com>
2025-11-20 07:26:54 +0100

    Introduce TextOptions with TextRenderingMode, TextHintingMode and BaselinePixelAlign

---
+++ src/Avalonia.Base/Media/TextHintingMode.cs
@@ -0,0 +1,30 @@
+﻿namespace Avalonia.Media
+{
+    public enum TextHintingMode : byte
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Unspecified,
+
+        /// <summary>
+        /// No hinting, outlines are scaled only.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Minimal hinting, preserves glyph shape, adjusts vertical metrics.
+        /// </summary>
+        Slight,
+
+        /// <summary>
+        /// Standard hinting, balances fidelity and readability.
+        /// </summary>
+        Normal,
+
+        /// <summary>
+        /// Aggressive grid-fitting, maximum crispness at low DPI.
+        /// </summary>
+        Full
+    }
+}
